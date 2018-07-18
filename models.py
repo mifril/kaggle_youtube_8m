9982ddcc4c2d1eb59a21aebd8e215711bc2a9a3a
Вахрушев Максим Игоревич <ma.vakhrushev@2gis.ru>
2018-07-18 16:47:39 +0700

    moe2

--- models.py
+++ models.py
@@ -160,9 +160,42 @@ def moe1(n_mixtures=3, dropout_val=0.3):
     return model
 
 
+def expert2(in_audio, in_rgb):
+    l_audio = fc_block(in_audio, 1024)
+    l_audio = fc_block(l_audio, 2048)
+    l_audio = fc_block(l_audio, 2048)
+
+    l_rgb = fc_block(in_rgb, 2048)
+    l_audio = fc_block(l_audio, 2048)
+    l_rgb = fc_block(l_rgb, 4096)
+
+    l_merge = concatenate([l_audio, l_rgb], axis=1)
+    l_merge = fc_block(l_merge, 4096)
+    l_merge = fc_block(l_merge, 4096)
+    return l_merge
+
+
+def moe2(n_mixtures=3, dropout_val=0.3):
+    in_audio = Input((128,), name='audio')
+    in_rgb = Input((1024,), name='rgb')
+    expert = expert2(in_audio, in_rgb)
+
+    expert_distribution = Dense(N_CLASSES * n_mixtures, activation='softmax', name='expert_activations')(expert)
+    expert_distribution = Reshape((-1, n_mixtures))(expert_distribution)
+
+    gate = concatenate([in_audio, in_rgb], axis=1)
+    gate_distribution = Dense(N_CLASSES * (n_mixtures + 1), activation='sigmoid', name='gate_activations')(gate)
+    gate_distribution = Reshape((-1, n_mixtures + 1))(gate_distribution)
+
+    out = Lambda(moe_out, name='output')([gate_distribution, expert_distribution])
+
+    model = Model(inputs=[in_audio, in_rgb], outputs=out)
+    return model
+
+
 def get_model_f(model_name):
     fmodels = {'dense1': dense1, 'dense2': dense2, 'dense3': dense3, 'dense4': dense4,
-               'moe1': moe1}
+               'moe1': moe1, 'moe2': moe2}
     return fmodels[model_name]
 
 
